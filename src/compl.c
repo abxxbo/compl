dfec65c9da4fea0b2159e88bdc9468456a174847
abxxbo <devavcado@gmail.com>
2022-09-18 12:21:40 -0500

    Begin rendering graph

--- src/compl.c
+++ src/compl.c
@@ -3,6 +3,24 @@
 
 #include <raylib.h>
 
+
+// Ignore this, this makes raylib not log anything
+void _(){}
+
 int main(void){
-  
+  SetTraceLogCallback(_);
+  InitWindow(640, 480, "COMPL");
+
+  while(!WindowShouldClose()){
+    // Draw
+    BeginDrawing();
+      ClearBackground(WHITE);
+
+      // Draw graph lines
+      DrawLine(640/2, 0, 640/2, 480, BLACK);
+      DrawLine(0, 480/2, 640, 480/2, BLACK);
+    EndDrawing();
+  }
+  CloseWindow();
+  return 0;
 }
