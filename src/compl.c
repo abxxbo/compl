d37b5e1d2048476680e4b87c9199123e4a82461b
abxxbo <devavcado@gmail.com>
2022-09-18 12:36:40 -0500

    render 1 point, but segfault

--- src/compl.c
+++ src/compl.c
@@ -10,6 +10,7 @@ typedef struct point {
 } point_t;
 
 #define MAX_PTS 256
+#define PTRADII 005   // Point radius
 
 
 // Ignore this, this makes raylib not log anything
@@ -30,9 +31,34 @@ int main(void){
     BeginDrawing();
       ClearBackground(WHITE);
 
+      // Plot all visible points
+      for(int i = 0; i != MAX_PTS; i++){
+        if(points[i].rendered == true){
+          DrawCircle(points[i].real_pos, points[i].imaginary_pos,
+                      PTRADII, BLACK);
+        }
+      }
+
       // Draw graph lines
       DrawLine(640/2, 0, 640/2, 480, BLACK);
       DrawLine(0, 480/2, 640, 480/2, BLACK);
+
+      DrawText(TextFormat("Re: %d\nIm: %d",
+                          GetMouseX(), GetMouseY()), 0, 0, 10, BLACK);
+
+      // If left click, and no point is visible at the mouse X, plot a point
+      if(IsMouseButtonPressed(MOUSE_BUTTON_LEFT)){
+        for(int i = 0; i != MAX_PTS; i++){
+          if(points[i].real_pos != GetMouseX() &&
+             points[i].real_pos != GetMouseY()){
+            points[cur_pt_iter].imaginary_pos = GetMouseY();
+            points[cur_pt_iter].real_pos      = GetMouseX();
+            points[cur_pt_iter].rendered      = true;
+            cur_pt_iter++;
+          } else continue;
+        }
+      }
+
     EndDrawing();
   }
   CloseWindow();
